91eb8b9b9cc66d9ea987565c0bb18b8d92fd00a6
Johan Sim Jian An <johansja@gmail.com>
2013-07-27 15:45:47 +0800

    Added list view to public.

--- qian/stocks/urls.py
+++ qian/stocks/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('stocks.views',
     url(r'^$', 'home', name='home'),
+    url(r"^categories/$", "categories"),
 )
--- qian/stocks/views.py
+++ qian/stocks/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
 
+from models import Category
+
 def home(request):
     return render(request, "stocks/home.html")
+
+def categories(request):
+    categories = Category.objects.all()
+    return render(request, "stocks/categories/home.html", {
+            "categories": categories,
+        })
